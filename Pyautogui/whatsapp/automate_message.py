233cba23879e0a7055fbbc7cfb5c05f380c16af5
vickyrules <vickyrules1705@gmail.com>
2022-10-23 16:15:52 +0530

    auto-message added

---
+++ Pyautogui/whatsapp/automate_message.py
@@ -0,0 +1,59 @@
+#please maximize your browsers window before running the program.
+#Do not close the browser in-between
+#Do not move cursor untill the program is running
+
+########## Use it justfor fun, not to bother someone#############
+#              '''Happpy Coding'''            #
+
+
+#install the following modules first
+# use the following terminal command
+
+# pip install pyautogui
+# pip install time
+# pip install sys
+from pyautogui import *
+from time import sleep
+import sys
+# import webbrowser as wb
+
+#add contacts names
+contacts = ["name"]
+
+#type your message here
+message = "hey Bro"
+
+#how many times you want to send
+send_cnt = 10
+
+# method to find the search bar location
+def click_search_name(name):
+    x1, y1 = [211,250]
+    moveTo(x1, y1)
+    click()
+    typewrite(name, interval=0.2)
+    sleep(2)
+    press('enter')
+
+# method to find and send message
+def click_send_message(msg):
+    x3, y3 = [950,750]
+    moveTo(x3, y3)
+    click()
+    sleep(2)
+    typewrite(msg)
+    press('enter')
+
+#iterating through the contact list
+
+for name in contacts:
+    try:
+        click_search_name(name)
+    except:
+        print("Unable to locate search bar or name")
+    try:
+        for i in range(100):
+            click_send_message(str(i)+". "+message)
+    except:
+        print("Unable to locate message bar")
+
